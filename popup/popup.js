da796052f4f24c8158b20589e4e344e94eb0801d
Andrew Lehman <aplehm@gmail.com>
2017-07-16 10:25:09 -0400

    Add quotes from me and caesar

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,28 @@
-async function main() {
+async function displayQuote(dogify) {
+  document.querySelector('p.quote').innerHTML = await getQuote(dogify);
+}
+
+async function getQuote(dogify = false) {
   const QUOTE_API_ENDPONT = 'https://talaikis.com/api/quotes/random/';
   const response = await fetch(QUOTE_API_ENDPONT);
   const jsonResponse = await response.json();
   const quote = jsonResponse.quote;
   const author = jsonResponse.author;
 
-  document.querySelector('p.quote').innerHTML = `${quote}<br/>-${author}`;
+  if(dogify) {
+    return dogifyQuote(quote);
+  }
+
+  return  `${quote}<br/>-${author}`;
+}
+
+function dogifyQuote(quote) {
+  let dogQuote = quote
+    .replace(/man|woman/g, 'dog')
+    .replace(/men|women/g, 'dogs')
+    .replace(/success|failure/g, 'bone');
+
+  return `${dogQuote}<br/>-Caesar`;
 }
 
 function addPortrait(subject) {
@@ -19,5 +36,14 @@ function addPortrait(subject) {
   document.querySelector('img.face').src = chrome.extension.getURL(image);
 }
 
-addPortrait('caesar');
-main();
+let andyPortraitInput = document.querySelector('input[value="andy"]');
+let caesarPortraitInput = document.querySelector('input[value="caesar"]');
+let portraitSelectEvent = (e) => {
+  addPortrait(e.target.value);
+  displayQuote((e.target.value === 'caesar'));
+};
+
+andyPortraitInput.addEventListener('click', (e) => portraitSelectEvent(e));
+caesarPortraitInput.addEventListener('click', (e) => portraitSelectEvent(e));
+addPortrait('andy');
+displayQuote();
