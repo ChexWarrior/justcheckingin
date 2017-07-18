b06018dc79c3e795cd41074055127cdd81b96053
Andrew Lehman <aplehm@gmail.com>
2017-07-17 20:08:21 -0400

    Add method to get random number

--- background.js
+++ background.js
@@ -17,6 +17,13 @@ function createAlarm() {
   });
 }
 
+async function getRandomNumber(min, max) {
+  const API_ENDPOINT = `https://www.random.org/integers/?num=1&max=${max}&min=${min}&base=10&format=plain&md=new`;
+  let response = await fetch(API_ENDPOINT);
+  
+  return await response.text();
+}
+
 browser.alarms.onAlarm.addListener((alarm) => {
   browser.notifications.create('test', {
     type: 'basic',
